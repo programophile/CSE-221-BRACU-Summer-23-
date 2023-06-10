caa6d2ed137d8e6a1de6dc7841e3bc90b35a0ead
Sayem- A ProgramoPhile <98210531+programophile@users.noreply.github.com>
2023-06-10 18:33:49 +0600

    task 4

---
+++ Lab 1/Task 4/Task 4.py
@@ -0,0 +1,28 @@
+def train_sort(n):
+    n=int(n)
+    arr=[i.strip() for i in input_file.readlines()]
+    str1=""
+    for i in range(n):
+        min_idx=i
+        for j in range (i+1,n):
+            if arr[min_idx].split()[0]>arr[j].split()[0]:
+                min_idx=j
+            elif arr[min_idx].split()[0]==arr[j].split()[0]:
+                if arr[min_idx].split()[-1].split(":")[0]<arr[j].split()[-1].split(":")[0]:
+                    min_idx=j
+                elif arr[min_idx].split()[-1].split(":")[0]==arr[j].split()[-1].split(":")[0]:
+                    if arr[min_idx].split()[-1].split(":")[1]<arr[j].split()[-1].split(":")[1]:
+                        min_idx = j
+        arr[i],arr[min_idx]=arr[min_idx],arr[i]
+
+    print(arr)
+    output_file.writelines([f"{line}\n" for line in arr])
+
+
+
+input_file=open("input4.txt","r")
+output_file=open("output4.txt",'w')
+
+train_sort(input_file.readline())
+input_file.close()
+output_file.close()
