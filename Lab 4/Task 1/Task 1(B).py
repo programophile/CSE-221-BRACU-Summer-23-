5915de6e6c9c58c668419ecde1752a4f3179fad5
Sayem- A ProgramoPhile <98210531+programophile@users.noreply.github.com>
2023-07-28 23:48:12 +0600

    algorithm

--- Lab 4/Task 1/Task 1(B).py
+++ Lab 4/Task 1/Task 1(B).py
@@ -1,4 +1,4 @@
-input_file=open('input1a_1.txt','r')
+input_file=open('input1b_1.txt','r')
 n,m=map(int,input_file.readline().split())
 graph={}
 for i in range(n+1):
@@ -12,6 +12,6 @@ str1=""
 for key,value in graph.items():
     str1+=f"{key} : {(' '.join((str(x) for x in value)))}"
     str1+="\n"
-output_file=open('output2a_1.txt','w')
+output_file=open('output1b_1.txt','w')
 output_file.write(str1)
 output_file.close()
